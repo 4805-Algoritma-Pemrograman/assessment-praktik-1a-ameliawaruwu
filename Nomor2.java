d4f6e72b50021532be7156821dfa6b72f42c3ea2
ameliawaruwu <ameliawaruwu50@gmail.com>
2024-10-24 10:43:01 +0900

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -1,5 +1,49 @@
-public class Nomor2{
-    public static void main(String[] args){
 
+import java.util.Scanner;
+public class asesmendua{
+        public static void main(String[] args) {
+           Scanner input  = new Scanner(System.in);
+
+            String namaDriver;
+            String inputLagi;
+            int lamaTrip;
+            int totalTrip;
+            int banyakPerjalanan = 0 ;
+            double biayaPerKm = 13000;
+            double totalOmzet ;
+            
+
+            System.out.print("Masukkan Nama Driver [Beliau/Mereka/Kita] : ");
+            namaDriver = input.next(); 
+
+            System.out.print("Lama Trip:");
+            lamaTrip = input.nextInt();
+            
+            System.out.print("Input Lagi [Y/N]: ");
+            inputLagi = input.next();
+
+            System.out.print("Total Trip yang Dilakukan: ");
+            
+
+            System.out.println("Omzet tertinggi yang diperoleh: ");
+            totalOmzet = banyakPerjalanan * biayaPerKm;
+
+     }
     }
-}
+
+
+Program Hitung Omzet 
+Var : integer;
+Var : double;
+Var nama : string;
+Algoritma
+INPUT nama driver
+INPUT lama trip
+OUTPUT lama trip
+OUTPUT omzet tertinggi
+END
+
+
+
+
+    
